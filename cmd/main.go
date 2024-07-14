584c1dae93923311c3b7e0863b19ce5164902b46
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-07-14 11:54:01 +0530

    Environment variables and db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/rohithrajasekharan/go-ecom/cmd/api"
+	"github.com/rohithrajasekharan/go-ecom/config"
+	"github.com/rohithrajasekharan/go-ecom/db"
 )
 
 func main() {
-	server := api.NewAPIServer(":8090", nil)
+	cfg := mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+	db, err := db.NewMySQLStorage(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8090", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB Successfully connected!")
 }
